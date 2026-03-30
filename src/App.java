b2d035362f7bfafc8c1963400e72335e6b3ef24e
J44v11 <javiergarcialopez101@gmail.com>
2026-03-30 18:28:53 +0100

    feat(App): Añado al personaje Eldrin

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
+import net.salesianos.personaje.Personaje;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        
+        Personaje viajero = new Personaje("Eldrin", 3);
+
+        viajero.entrenar(4);
+        viajero.gastarEnergia(30);
+        viajero.mostrarInfo();
+        Personaje.mostrarTotalPersonajes();
+
     }
 }
