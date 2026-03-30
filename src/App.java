ed9b575fb32f96b2d204ce057b19f74990f37ab9
J44v11 <javiergarcialopez101@gmail.com>
2026-03-30 18:32:40 +0100

    feat(App): Añado al personaje Lyra

--- src/App.java
+++ src/App.java
@@ -1,4 +1,5 @@
 import net.salesianos.personaje.Guerrero;
+import net.salesianos.personaje.Mago;
 import net.salesianos.personaje.Personaje;
 
 public class App {
@@ -12,12 +13,20 @@ public class App {
         viajero.mostrarInfo();
         Personaje.mostrarTotalPersonajes();
 
-        
+
         Guerrero thorak = new Guerrero("Thorak", 5, "Hacha doble");
 
         thorak.ataquePesado();
         thorak.descansar(10);
         thorak.mostrarInfo();
+        Personaje.mostrarTotalPersonajes();
+
+
+        Mago lyra = new Mago("Lyra", 4, "Hielo");
+        
+        lyra.ataqueElemental();
+        lyra.entrenar(2);
+        lyra.mostrarInfo();
         Personaje.mostrarTotalPersonajes();
 
     }
